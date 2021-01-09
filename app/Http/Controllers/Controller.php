4118beca1c5f3439914569f9e220c8de335ab4bf
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2021-01-09 19:52:53 -0300

    [change] Função de retornar resposta ao front adicionada no Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /** Para facilitar retornos ao front 
+     * @param data array
+     * @param message string
+     * @param code integer
+    */
+    protected function response($data, $message, $code){
+        return response()->json([
+            'Data' => $data,
+            'Message' => $message,
+            'Code' => $code
+        ], $code);
+    }
 }
