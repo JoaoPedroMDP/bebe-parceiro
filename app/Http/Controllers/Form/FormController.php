37e48b1b67b60d5be85e2d487a673003b6b1f343
JoaoPedroMDP <joao.pedro.mdp@outlook.com>
2021-01-12 19:54:39 -0300

    [fix] Importação do 'Controller' corrigida

--- app/Http/Controllers/Form/FormController.php
+++ app/Http/Controllers/Form/FormController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Form;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\FormValidationRequest;
 class FormController extends Controller
